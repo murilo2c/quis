7ede2f11a1ada93fc9d20878980ccf186e1b38fa
murilo2c <168766721+murilo2c@users.noreply.github.com>
2024-11-14 14:29:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Animais',
+    'Qual animal terrestre mais forte do mundo?',
+    'O animal mais forte do mundo é o elefante-africano'
+)
+
+criaCartao(
+    'Animais',
+    'Qual o anialmais inteligente do mundo?',
+    'O animal masi inteligente do mundo é o chinpanzé'
+)
+
+criaCartao(
+    'Animais',
+    'Qual o animal as perigoso do mundo?',
+    'O animal considerado omais perigososo mundo é a vespa-do-mar'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Onde apareceu o primeiro chuvero elétrico?',
+    'No Brasil Francisco Canhos desenvolveu o primeiro chuveiro elétrico seguro'
+)
